fd75d1aa37beeeafd328270bdfe307c199edf719
Carlos Guedes <cguedes@gmail.com>
2023-05-16 11:50:00 +0100

    Adds Lexical with plain text editor
    - integration with AI panel for selected text

--- src/EditorPanel.tsx
+++ src/EditorPanel.tsx
@@ -1,32 +1,120 @@
 import { Panel } from 'react-resizable-panels';
+import {
+  $createParagraphNode,
+  $createTextNode,
+  $getRoot,
+  $getSelection,
+  EditorState,
+  EditorThemeClasses,
+} from 'lexical';
+import { useEffect } from 'react';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { InitialConfigType } from '@lexical/react/LexicalComposer';
+
+// Lexical React plugins are React components, which makes them
+// highly composable. Furthermore, you can lazy load plugins if
+// desired, so you don't pay the cost for plugins until you
+// actually use them.
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error: Error) {
+  console.error(error);
+}
 
 export function EditorPanel({
   onSelection,
 }: {
   onSelection: (selection: string) => any;
 }) {
+  const initialConfig: InitialConfigType = {
+    namespace: 'RefStudioEditor',
+    onError,
+    editorState: prepopulatedRichText,
+  };
+
+  // When the editor changes, you can get notified via the
+  // LexicalOnChangePlugin!
+  function onChange(editorState: EditorState) {
+    editorState.read(() => {
+      // Read the contents of the EditorState here.
+      const root = $getRoot();
+      const selection = $getSelection();
+      console.log(root, selection);
+
+      const selectedText = selection?.getTextContent();
+      selectedText && onSelection(selectedText);
+    });
+  }
+
   return (
     <Panel defaultSize={60}>
       <h1>Editor</h1>
-      <div>
-        At last the Mouse, who seemed to be two people! `I must be getting
-        somewhere near the centre of the bottle was NOT marked `poison,' so
-        Alice ventured to taste it, and fortunately was just in time to see it
-        written up somewhere.' Down, down, down. I am so VERY remarkable in
-        that; nor did Alice think it so VERY remarkable in that; nor did Alice
-        think it so VERY tired of sitting by her sister was reading, but it had
-        no pictures or conversations in it, `and what is the use of a book,'
-        thought Alice `without pictures or conversation?' So she was to get
-        rather sleepy, and went on saying to herself, `Which way? `We won't talk
-        about her any more if you'd rather not.' `We indeed!' cried the Mouse,
-        who seemed to be ashamed of yourself,' said Alice, `a great girl like
-        you,' (she might well say this), `to go on crying in this way! I shall
-        only look up and picking the daisies, when suddenly a White Rabbit
-        returning, splendidly dressed, with a pair of white kid gloves while she
-        was talking. Ah, THAT'S the great puzzle!' And she began thinking over
-        all the rats and--oh dear!' cried Alice in a soothing tone: `don't be
-        angry about it.
-      </div>
+      <LexicalComposer initialConfig={initialConfig}>
+        <PlainTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<div></div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <OnChangePlugin onChange={onChange} />
+        <HistoryPlugin />
+        <MyCustomAutoFocusPlugin />
+      </LexicalComposer>
     </Panel>
   );
+}
+
+function prepopulatedRichText() {
+  const root = $getRoot();
+  if (root.getFirstChild() === null) {
+    const paragraph = $createParagraphNode();
+    paragraph.append(
+      $createTextNode('The playground is a demo environment built with '),
+      $createTextNode('@lexical/react').toggleFormat('code'),
+      $createTextNode('.'),
+      $createTextNode(' Try typing in '),
+      $createTextNode('some text').toggleFormat('bold'),
+      $createTextNode(' with '),
+      $createTextNode('different').toggleFormat('italic'),
+      $createTextNode(' formats.'),
+    );
+    root.append(paragraph);
+    const paragraph2 = $createParagraphNode();
+    paragraph2.append(
+      $createTextNode(
+        'Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!',
+      ),
+    );
+    root.append(paragraph2);
+    const paragraph3 = $createParagraphNode();
+    paragraph3.append(
+      $createTextNode(`If you'd like to find out more about Lexical, you can:`),
+    );
+    root.append(paragraph3);
+
+    const paragraph4 = $createParagraphNode();
+    paragraph4.append(
+      $createTextNode(
+        `Lastly, we're constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).`,
+      ),
+    );
+    root.append(paragraph4);
+  }
 }
