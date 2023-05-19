7de9977a1f3b961521a9774062b88dfa98b2153f
Mathis Pierron <mathis.pierron@yld.io>
2023-05-19 12:06:23 +0100

    Run prettier

--- src/CollapsibleBlock/CollapsibleBlock.tsx
+++ src/CollapsibleBlock/CollapsibleBlock.tsx
@@ -6,36 +6,43 @@ import './styles.css';
 
 // The attributes in this interface must correspond to the attributes defined in the `addAttributes` method of CollapsibleBlockNode
 export interface CollapsibleBlockProps {
-    updateAttributes: (attributes: CollapsibleBlockNodeAttributes) => void
-    node: Node & { attrs: CollapsibleBlockNodeAttributes }
+  updateAttributes: (attributes: CollapsibleBlockNodeAttributes) => void;
+  node: Node & { attrs: CollapsibleBlockNodeAttributes };
 }
 
 export const CollapsibleBlock = (props: CollapsibleBlockProps) => {
-    const handleButtonClick = () => {
-        props.updateAttributes({
-            folded: !props.node.attrs.folded,
-        });
-    };
-    return (
-        <NodeViewWrapper>
-            <div className='draggable-item collapsible-block' style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
-                <div
-                    className="drag-handle"
-                    contentEditable="false"
-                    draggable="true"
-                    data-drag-handle
-                />
-                <button
-                    className={classNames({
-                        'folded': props.node.attrs.folded
-                    })}
-                    onClick={handleButtonClick}
-                />
+  const handleButtonClick = () => {
+    props.updateAttributes({
+      folded: !props.node.attrs.folded,
+    });
+  };
+  return (
+    <NodeViewWrapper>
+      <div
+        className="draggable-item collapsible-block"
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+        }}
+      >
+        <div
+          className="drag-handle"
+          contentEditable="false"
+          draggable="true"
+          data-drag-handle
+        />
+        <button
+          className={classNames({
+            folded: props.node.attrs.folded,
+          })}
+          onClick={handleButtonClick}
+        />
 
-                <NodeViewContent
-                    className={classNames('content', { 'folded': props.node.attrs.folded })}
-                />
-            </div>
-        </NodeViewWrapper>
-    );
+        <NodeViewContent
+          className={classNames('content', { folded: props.node.attrs.folded })}
+        />
+      </div>
+    </NodeViewWrapper>
+  );
 };
--- src/CollapsibleBlock/nodes/CollapsibleBlockContent.ts
+++ src/CollapsibleBlock/nodes/CollapsibleBlockContent.ts
@@ -1,9 +1,9 @@
-import { mergeAttributes, Node } from "@tiptap/core";
+import { mergeAttributes, Node } from '@tiptap/core';
 
 export const CollapsibleBlockContentNode = Node.create({
-  name: "collapsibleContent",
-  group: "block",
-  content: "block+",
+  name: 'collapsibleContent',
+  group: 'block',
+  content: 'block+',
 
   parseHTML() {
     return [
