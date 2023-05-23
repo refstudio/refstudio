6eac87ad56ea8e84f71c8f9277dd8a6be090bb6f
Carlos Guedes <cguedes@gmail.com>
2023-05-23 16:35:13 +0100

    Update filesystem base dir to appDataDir instead of homeDir

--- src/filesystem.tsx
+++ src/filesystem.tsx
@@ -5,9 +5,9 @@ import {
   readBinaryFile,
   readDir,
   writeBinaryFile,
-  writeTextFile
+  writeTextFile,
 } from '@tauri-apps/api/fs';
-import { homeDir, join } from '@tauri-apps/api/path';
+import { appDataDir, join } from '@tauri-apps/api/path';
 
 import { INITIAL_CONTENT } from './TipTapEditor/TipTapEditorConfigs';
 
@@ -15,7 +15,7 @@ const REF_STUDIO_DIR = '.ref-studio/project-x';
 const UPLOADS_DIR = 'uploads';
 
 async function getBaseDir() {
-  return join(await homeDir(), REF_STUDIO_DIR);
+  return join(await appDataDir(), REF_STUDIO_DIR);
 }
 export async function ensureProjectFileStructure() {
   try {
