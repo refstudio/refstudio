c9b9768dc2beda2119277d85dc953ba767b815da
Greg Reda <gjreda+github@gmail.com>
2023-05-23 14:38:28 -0700

    fix reset script

--- scripts/reset_uploads.sh
+++ scripts/reset_uploads.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm $HOME/.ref-studio/project-x/uploads/*.pdf
-rm -rf ~/.ref-studio/project-x/xml
+rm -rf ~/.ref-studio/project-x/grobid
 rm -rf ~/.ref-studio/project-x/storage
 rm -rf ~/.ref-studio/project-x/.lancedb
