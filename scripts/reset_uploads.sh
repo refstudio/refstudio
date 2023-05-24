a1ee5397e73827ea3fa59b22f1cc10b8a1909466
Greg Reda <gjreda+github@gmail.com>
2023-05-24 11:15:46 -0700

    fix reset script

--- scripts/reset_uploads.sh
+++ scripts/reset_uploads.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-rm $HOME/.ref-studio/project-x/uploads/*.pdf
-rm -rf $HOME/.ref-studio/project-x/grobid
-rm -rf $HOME/.ref-studio/project-x/storage
-rm -rf $HOME/.ref-studio/project-x/.lancedb
+PROJECT_DIR="/Users/greg/Library/Application Support/com.tauri.dev/project-x"
+
+rm "$PROJECT_DIR/uploads/"*.pdf
+rm -rf "$PROJECT_DIR/grobid"
+rm -rf "$PROJECT_DIR/storage"
+rm -rf "$PROJECT_DIR/.lancedb"
