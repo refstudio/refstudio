0f96b7da9af0ad70763925bbdfb2c5e3cf33a3e9
Greg Reda <gjreda+github@gmail.com>
2023-05-24 16:16:55 -0700

    fix

--- src-python/main.py
+++ src-python/main.py
@@ -1,9 +1,10 @@
-from argparse import ArgumentParser
-from pathlib import Path
-from grobid_tei_xml.types import GrobidDocument
-import grobid_tei_xml
 import json
 import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
+import grobid_tei_xml
+from grobid_tei_xml.types import GrobidDocument
 
 
 def parse_xml_file(filepath: Path) -> GrobidDocument:
@@ -24,7 +25,7 @@ def main(text: str):
     try:
         data = {"num_words": get_word_count(text)}
         print(json.dumps(data))
-    except:
+    except Exception:
         print("Error processing text", file=sys.stderr)
 
 
