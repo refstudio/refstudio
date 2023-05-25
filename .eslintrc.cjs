fc183a5cb4a2ee55e759a1262f039213e435c8ee
Dan Vanderkam <danvdk@gmail.com>
2023-05-25 14:30:31 -0400

    try some more rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
+    project: ['tsconfig.json'],
     "ecmaVersion": 12,
     "sourceType": "module"
   },
@@ -28,5 +29,46 @@ module.exports = {
   "env": {
     "browser": true,
     "es2021": true
+  },
+  "rules": {
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/consistent-type-assertions': 'error',
+    '@typescript-eslint/consistent-type-definitions': 'error',
+    '@typescript-eslint/dot-notation': 'error',
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/no-empty-function': 'error',
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-function-type': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {allowTemplateLiterals: true, avoidEscape: true},
+    ],
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/triple-slash-reference': [
+      'error',
+      {
+        path: 'always',
+        types: 'prefer-import',
+        lib: 'always',
+      },
+    ],
+    '@typescript-eslint/unified-signatures': 'error',
+    'arrow-body-style': 'warn',
+    'brace-style': ['error', '1tbs'],
+    camelcase: 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    complexity: 'off',
+    'constructor-super': 'error',
+    curly: 'error',
+    'eol-last': 'error',
+    eqeqeq: ['error', 'smart'],
+    'guard-for-in': 'error',
   }
 };
