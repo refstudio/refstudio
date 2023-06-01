b7c98cf9e212202f537d6a5e148fb71937a1d085
Carlos Guedes <cguedes@gmail.com>
2023-06-01 13:20:07 +0100

    Improve editor styles

--- src/TipTapEditor/MenuBar.tsx
+++ src/TipTapEditor/MenuBar.tsx
@@ -35,7 +35,7 @@ export function MenuBar({ editor }: { editor: Editor }) {
   }, [editor, refreshToggle]);
 
   return (
-    <menu className="toolbar menu-bar x-hide-scrollbars m-5 mb-0 overflow-x-scroll">
+    <menu className="toolbar menu-bar x-hide-scrollbars">
       <button
         className="toolbar-item"
         disabled={!editor.can().chain().focus().undo().run()}
