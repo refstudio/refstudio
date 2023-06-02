27c44cdf63e2e3e2c7f00a991e27d331cf0dd5fb
Greg Reda <gjreda+github@gmail.com>
2023-06-02 11:49:25 -0700

    Forgot to update this as part of #72 and #73

--- scripts/reset_uploads.sh
+++ scripts/reset_uploads.sh
@@ -3,6 +3,6 @@
 PROJECT_DIR="/Users/greg/Library/Application Support/com.tauri.dev/project-x"
 
 rm "$PROJECT_DIR/uploads/"*.pdf
-rm -rf "$PROJECT_DIR/grobid"
-rm -rf "$PROJECT_DIR/storage"
+rm -rf "$PROJECT_DIR/.grobid"
+rm -rf "$PROJECT_DIR/.storage"
 rm -rf "$PROJECT_DIR/.lancedb"
