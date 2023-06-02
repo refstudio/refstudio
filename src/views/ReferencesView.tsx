f4a542b4b2af74a99b9c2d2359ca34659e07e384
Mathis Pierron <mathis.pierron@yld.io>
2023-06-02 16:33:18 +0100

    feat: use references atom in references view

--- src/views/ReferencesView.tsx
+++ src/views/ReferencesView.tsx
@@ -1,68 +1,43 @@
+
+import { useAtomValue } from 'jotai';
+
+import { getReferencesAtom, isPdfIngestionRunningAtom } from '../atoms/referencesState';
+import { Spinner } from '../components/Spinner';
 import { ReferenceItem } from '../types/ReferenceItem';
 
-export function ReferencesView({ onRefClicked }: { onRefClicked?: (item: ReferenceItem) => void }) {
+interface ReferencesViewProps {
+  onRefClicked: (item: ReferenceItem) => void;
+}
+
+export function ReferencesView({ onRefClicked }: ReferencesViewProps) {
+  const references = useAtomValue(getReferencesAtom);
+  const isPdfIngestionLoading = useAtomValue(isPdfIngestionRunningAtom);
+
   return (
     <div className="flex h-full w-full flex-col">
       <h1>References</h1>
-      <div className="flex-1 overflow-scroll">
+      <div className="flex-1 overflow-scroll flex flex-col">
         <p className="my-4 italic">Click on a reference to add it to the document.</p>
 
         <ul className="divide-y-2 border">
-          {REFS_DATABASE.map((reference) => (
+          {references.map((reference) => (
             <li
-              className="mb-0 cursor-pointer p-1 hover:bg-slate-100"
+              className="mb-0 cursor-pointer p-1 hover:bg-slate-100 overflow-x-hidden text-ellipsis"
               key={reference.id}
-              onClick={() => onRefClicked?.(reference)}
+              onClick={() => onRefClicked(reference)}
             >
-              <strong>{reference.id}</strong> - {reference.title}
+              <strong>{reference.title}</strong>
               <br />
               <small>
-                <em>
-                  {reference.author}, {reference.year} by {reference.publisher}
+                <em className="whitespace-nowrap">
+                  {reference.authors.map(({ fullName }) => fullName).join(', ')}
                 </em>
               </small>
             </li>
           ))}
         </ul>
+        {isPdfIngestionLoading && <Spinner />}
       </div>
     </div>
   );
 }
-
-const REFS_DATABASE: ReferenceItem[] = [
-  {
-    id: 'ref1',
-    title: 'Artificial Intelligence: A Modern Approach',
-    author: 'Stuart Russell, Peter Norvig',
-    year: 2016,
-    publisher: 'Pearson',
-  },
-  {
-    id: 'ref2',
-    title: 'Deep Learning',
-    author: 'Ian Goodfellow, Yoshua Bengio, Aaron Courville',
-    year: 2016,
-    publisher: 'MIT Press',
-  },
-  {
-    id: 'ref3',
-    title: 'Machine Learning: A Probabilistic Perspective',
-    author: 'Kevin P. Murphy',
-    year: 2012,
-    publisher: 'MIT Press',
-  },
-  {
-    id: 'ref4',
-    title: 'Pattern Recognition and Machine Learning',
-    author: 'Christopher M. Bishop',
-    year: 2006,
-    publisher: 'Springer',
-  },
-  {
-    id: 'ref5',
-    title: 'Reinforcement Learning: An Introduction',
-    author: 'Richard S. Sutton, Andrew G. Barto',
-    year: 2018,
-    publisher: 'MIT Press',
-  },
-];
