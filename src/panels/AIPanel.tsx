9eee90ba2d93236a9336ed0ada0820d0f7d97a0c
Carlos Guedes <cguedes@gmail.com>
2023-06-07 12:08:18 +0100

    Adjust AI View to use rewrite sidecar command

--- src/panels/AIPanel.tsx
+++ src/panels/AIPanel.tsx
@@ -8,19 +8,29 @@ import { PanelSection } from '../components/PanelSection';
 import { PanelWrapper } from '../components/PanelWrapper';
 
 export function AIPanel() {
-  const [reply, setReply] = useState('');
-
   const selection = useAtomValue(selectionAtom);
   const debouncedSelection = useDebounce(selection, 200);
 
   useEffect(() => {
-    if (!selection) {
-      return;
-    }
-    interactWithAi(selection)
-      .then((num) => setReply(String(num)))
-      .catch(console.error);
-  }, [selection]);
+    setRewriteReply('');
+  }, [debouncedSelection]);
+
+  const [rewriteReply, setRewriteReply] = useState('');
+  const [loadingRewrite, setLoadingRewrite] = useState(false);
+
+  function handleRewriteClick() {
+    (async function run() {
+      try {
+        setLoadingRewrite(true);
+        const reply = await askForRewrite(selection);
+        setRewriteReply(reply);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoadingRewrite(false);
+      }
+    })();
+  }
 
   return (
     <PanelWrapper title="AI">
@@ -30,8 +40,22 @@ export function AIPanel() {
         {selection && (
           <div className="flex flex-col gap-4">
             <div className="border border-yellow-100 bg-yellow-50 p-4">{debouncedSelection}</div>
-            <strong>REPLY</strong>
-            <div className="border border-green-100 bg-green-50 p-4">{reply}</div>
+            <button
+              className="btn rounded-xl bg-yellow-100 hover:bg-yellow-200"
+              disabled={loadingRewrite}
+              onClick={handleRewriteClick}
+            >
+              REWRITE
+            </button>
+            {loadingRewrite && <span>Processing...</span>}
+            {rewriteReply && (
+              <>
+                <div className="border border-green-100 bg-green-50 p-4">{rewriteReply}</div>
+                {/* <button className="btn rounded-xl bg-green-100 hover:bg-green-200" onClick={handleReplaceClicked}>
+                  REPLACE
+                </button> */}
+              </>
+            )}
           </div>
         )}
       </PanelSection>
@@ -39,23 +63,17 @@ export function AIPanel() {
   );
 }
 
-interface AIResponse {
-  num_words: number;
-}
-
-async function interactWithAi(selection: string) {
+async function askForRewrite(selection: string) {
   try {
-    const command = new Command('call-sidecar', ['ai', '--text', `${selection}`]);
+    const command = new Command('call-sidecar', ['rewrite', '--text', `${selection}`]);
     console.log('command', command);
     const output = await command.execute();
-    console.log('output', output);
-
     if (output.stderr) {
       throw new Error(output.stderr);
     }
-
-    const response = JSON.parse(output.stdout) as AIResponse;
-    const aiReply = response.num_words;
+    console.log('output: ', output.stdout);
+    const response = JSON.parse(output.stdout) as { index: number; text: string }[];
+    const aiReply = response[0].text;
     return aiReply;
   } catch (reason) {
     throw new Error(`ERROR: ${reason}`);
