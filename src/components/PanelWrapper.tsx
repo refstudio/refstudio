cf7cb27524a0bfefad03e87e74eefecb0308789c
Carlos Guedes <cguedes@users.noreply.github.com>
2023-06-07 14:37:21 +0100

    feat: left/right collapsible sidebars (#93)
    Fixes #68

--- src/components/PanelWrapper.tsx
+++ src/components/PanelWrapper.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { VscClose } from 'react-icons/vsc';
 
-export function PanelWrapper({ title, children }: { title: string; children: React.ReactNode }) {
+export function PanelWrapper({
+  title,
+  closable = false,
+  onCloseClick,
+  children,
+}: {
+  title: string;
+  closable?: boolean;
+  onCloseClick?: () => void;
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex h-full flex-col">
-      <div className="flex flex-shrink-0 flex-grow-0 select-none border-b-2 border-t-2 border-slate-300 p-2 text-sm text-black">
+      <div className="flex flex-shrink-0 flex-grow-0 select-none items-center border-b-2 border-t-2 border-slate-300 p-2 text-sm text-black">
         <div>{title.toUpperCase()}</div>
-        <div className="ml-auto">{/* NOTE: This is a placeholder element for wrapper actions */}</div>
+        <div className="ml-auto">
+          {closable && <VscClose className="cursor-pointer" onClick={() => onCloseClick?.()} />}
+        </div>
       </div>
       <div className="flex flex-col gap-1 overflow-hidden pt-2">{children}</div>
     </div>
