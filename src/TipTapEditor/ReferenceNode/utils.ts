646d1234338068fa4db6c18f0be713d48dfc7e59
Mathis Pierron <mathis.pierron@yld.io>
2023-06-07 17:12:13 +0100

    fix: typo

--- src/TipTapEditor/ReferenceNode/utils.ts
+++ src/TipTapEditor/ReferenceNode/utils.ts
@@ -2,11 +2,11 @@ import { ReferenceItem } from '../../types/ReferenceItem';
 
 /**
  * Defines the rules to apply to filter references in the selector
- * @param query string query to filter on
+ * @param query query string to filter on
  * @returns the filter function
  */
 export function getReferenceFilter(query: string) {
   return (referenceItem: ReferenceItem) =>
     referenceItem.title.toLowerCase().includes(query.toLowerCase()) ||
-    referenceItem.authors.some(({ fullName }) => fullName.toLowerCase().includes(query));
+    referenceItem.authors.some(({ fullName }) => fullName.toLowerCase().includes(query.toLowerCase()));
 }
