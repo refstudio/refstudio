85a84925ded6927d4ada72216c186bee0ed62be4
Mathis Pierron <58954208+sehyod@users.noreply.github.com>
2023-06-09 11:49:07 +0100

    fix: disallow transparent text in the editor (#116)
    Co-authored-by: Mathis Pierron <mathis.pierron@yld.io>
    
    fixes #115

--- src/TipTapEditor/TipTapEditor.tsx
+++ src/TipTapEditor/TipTapEditor.tsx
@@ -9,7 +9,7 @@ import { EditorAPI } from '../types/EditorAPI';
 import { MenuBar } from './MenuBar';
 import { ReferenceNode } from './ReferenceNode/ReferenceNode';
 import { getReferenceLabel } from './ReferenceNode/ReferencesList';
-import { EDITOR_EXTENSIONS, INITIAL_CONTENT } from './TipTapEditorConfigs';
+import { EDITOR_EXTENSIONS, INITIAL_CONTENT, transformPasted } from './TipTapEditorConfigs';
 
 interface EditorProps {
   editorRef: React.MutableRefObject<EditorAPI | null>;
@@ -30,6 +30,9 @@ export function TipTapEditor({ editorRef, editorContent }: EditorProps) {
           const { from, to } = newEditor.view.state.selection;
           const text = newEditor.view.state.doc.textBetween(from, to);
           setSelection(text);
+        },
+        editorProps: {
+          transformPasted,
         },
       }),
     );
