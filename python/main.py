0d1c28e07faf45ffa317f44c714d70583a004a95
Carlos Guedes <cguedes@gmail.com>
2023-06-09 12:55:59 +0100

    Fix main

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ from sidecar import cli, ingest, rewrite
 
 if __name__ == '__main__':
     parser = cli.get_arg_parser()
-    args = parser._get_args()
+    args = parser.parse_args()
     if args.debug:
         print(args)
     if args.command == "ingest":
