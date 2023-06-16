8cdd25a517bb8bd6dc14363114661fc6b84b8ffa
Carlos Guedes <cguedes@gmail.com>
2023-06-16 13:37:05 +0100

    implement own empty function instead of vi.fn()

--- src/components/PrimarySideBar.test.tsx
+++ src/components/PrimarySideBar.test.tsx
@@ -1,31 +1,33 @@
 import { render, screen, userEvent } from '../utils/test-utils';
 import { PrimarySideBar } from './PrimarySideBar';
 
-const emptyFn = vi.fn();
+const noop = () => {
+  // Empty
+};
 
 describe('PrimarySideBar', () => {
   test('should display Explorer and References icons', () => {
-    render(<PrimarySideBar activePane="Explorer" onClick={emptyFn} onSettingsClick={emptyFn} />);
+    render(<PrimarySideBar activePane="Explorer" onClick={noop} onSettingsClick={noop} />);
     expect(screen.getByRole('menubar')).toBeInTheDocument();
     expect(screen.getByRole('menuitem', { name: 'Explorer' })).toBeInTheDocument();
     expect(screen.getByRole('menuitem', { name: 'References' })).toBeInTheDocument();
   });
 
   test('should display active icon with 100% opacity ', () => {
-    render(<PrimarySideBar activePane="References" onClick={emptyFn} onSettingsClick={emptyFn} />);
+    render(<PrimarySideBar activePane="References" onClick={noop} onSettingsClick={noop} />);
     expect(screen.getByRole('menubar')).toBeInTheDocument();
     expect(screen.getByRole('menuitem', { name: 'References' })).toHaveClass('opacity-100');
   });
 
   test('should display inactive icon with 50% opacity ', () => {
-    render(<PrimarySideBar activePane="References" onClick={emptyFn} onSettingsClick={emptyFn} />);
+    render(<PrimarySideBar activePane="References" onClick={noop} onSettingsClick={noop} />);
     expect(screen.getByRole('menubar')).toBeInTheDocument();
     expect(screen.getByRole('menuitem', { name: 'Explorer' })).toHaveClass('opacity-50');
   });
 
   test('should call onClick with pane name', async () => {
     const fn = vi.fn();
-    render(<PrimarySideBar activePane="References" onClick={fn} onSettingsClick={emptyFn} />);
+    render(<PrimarySideBar activePane="References" onClick={fn} onSettingsClick={noop} />);
     await userEvent.setup().click(screen.getByRole('menuitem', { name: 'Explorer' }));
     expect(fn).toBeCalled();
     expect(fn).toBeCalledWith('Explorer');
@@ -33,7 +35,7 @@ describe('PrimarySideBar', () => {
 
   test('should call onSettingsClick with click in the settings icon', async () => {
     const settingsFn = vi.fn();
-    render(<PrimarySideBar activePane="References" onClick={emptyFn} onSettingsClick={settingsFn} />);
+    render(<PrimarySideBar activePane="References" onClick={noop} onSettingsClick={settingsFn} />);
     await userEvent.setup().click(screen.getByRole('menuitem', { name: 'Settings' }));
     expect(settingsFn).toBeCalled();
   });
