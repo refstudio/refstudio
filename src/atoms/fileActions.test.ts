c79e4c41d1c4bd5930964262ce03ee3035b7e5ce
Mathis Pierron <mathis.pierron@yld.io>
2023-06-19 14:07:08 +0100

    Revert "skip test"
    This reverts commit 20a74e521469abdc3bc08951b430847ea7f6611a.

--- src/atoms/fileActions.test.ts
+++ src/atoms/fileActions.test.ts
@@ -23,7 +23,7 @@ describe('fileActions', () => {
     vi.clearAllMocks();
   });
 
-  test.skip('should be empty by default', () => {
+  test('should be empty by default', () => {
     const store = createStore();
     const leftPane = runGetAtomHook(leftPaneAtom, store);
     const rightPane = runGetAtomHook(rightPaneAtom, store);
