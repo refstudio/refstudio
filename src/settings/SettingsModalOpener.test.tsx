c86e94bddb0bd1d1eb0b631355271b7665eede3f
Carlos Guedes <cguedes@gmail.com>
2023-06-20 11:44:05 +0100

    Remove @testing-library/react-hooks dependency
    Fix issue https://github.com/refstudio/refstudio/issues/158

--- src/settings/SettingsModalOpener.test.tsx
+++ src/settings/SettingsModalOpener.test.tsx
@@ -1,4 +1,4 @@
-import { act } from '@testing-library/react-hooks';
+import { act } from '@testing-library/react';
 
 import { listenEvent, RefStudioEvents } from '../events';
 import { noop } from '../utils/noop';
