96bc8c1f0fe25cfc1a440670b45893fd91848804
Mathis Pierron <mathis.pierron@yld.io>
2023-06-23 14:08:58 +0100

    feat: add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'jotai';
 import React from 'react';
@@ -11,10 +12,12 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </Provider>
+    <ChakraProvider>
+      <Provider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
