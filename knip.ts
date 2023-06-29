592ac1793ed057fcbf0f2c8e2dd5c33d59d8a8ad
Dan Vanderkam <danvdk@gmail.com>
2023-06-29 12:10:41 -0400

    four

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   entry: ['src/main.tsx'],
   project: ['src/**/*.ts', 'src/**/*.tsx'],
-  ignore: ['src/api/types.ts'],
+  ignore: ['src/api/types.ts', 'src/vite-env.d.ts'],
   exclude: ['types'],
 };
 
