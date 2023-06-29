fbb1b0b001bacbbb6847aa91f9cbca6777344429
Dan Vanderkam <danvdk@gmail.com>
2023-06-29 13:05:06 -0400

    Add to workflow with a false positive

--- knip.ts
+++ knip.ts
@@ -4,8 +4,14 @@ const config: KnipConfig = {
   entry: ['src/main.tsx'],
   project: ['src/**/*.ts', 'src/**/*.tsx'],
   ignore: ['src/api/types.ts', 'src/vite-env.d.ts'],
-  exclude: ['types'],
   ignoreBinaries: ['poetry'],
+  ignoreExportsUsedInFile: true,
+  ignoreDependencies: [
+    // referenced in scripts/codegen.sh
+    'json-schema-to-typescript',
+    // Prettier auto-loads this plugin, see https://github.com/webpro/knip/issues/70
+    'prettier-plugin-tailwindcss',
+  ],
 };
 
 export default config;
