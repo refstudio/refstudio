5fa606a264d9a9cd5aa45b15ed9efda0658aff6e
Dan Vanderkam <danvdk@gmail.com>
2023-06-30 06:09:43 -0400

    chore: Put TS tests in `__tests__` (Jest style) (#230)
    * move src/api
    
    * src/atoms
    
    * src/components
    
    * src/features/references
    
    * tipTapNodes
    
    * settings
    
    * wrappers
    
    * save all
    
    * vi.mock is a pain
    
    * 28 tests failling
    
    * a few more
    
    * 3 failing test files
    
    * one more
    
    * All tests passing
    
    * Move remaining tests into __tests__
    
    * rescue one non-test
    
    * sort
    
    ---------
    
    Co-authored-by: Carlos Guedes <cguedes@gmail.com>

---
+++ src/io/__tests__/readEnv.test.ts
@@ -0,0 +1,25 @@
+import { invoke } from '@tauri-apps/api';
+
+import { readEnv } from '../readEnv';
+
+vi.mock('@tauri-apps/api');
+
+describe('readEnv', () => {
+  it('should return env value from tauri', async () => {
+    vi.mocked(invoke).mockResolvedValue('TAURI ENV VALUE');
+    const value = await readEnv('KEY', 'unused');
+    expect(value).toBe('TAURI ENV VALUE');
+  });
+
+  it('should fallback if env value is empty', async () => {
+    vi.mocked(invoke).mockResolvedValue('');
+    const value = await readEnv('KEY', 'FALLBACK');
+    expect(value).toBe('FALLBACK');
+  });
+
+  it('should fallback if throw', async () => {
+    vi.mocked(invoke).mockRejectedValue('some error');
+    const value = await readEnv('KEY', 'FALLBACK');
+    expect(value).toBe('FALLBACK');
+  });
+});
