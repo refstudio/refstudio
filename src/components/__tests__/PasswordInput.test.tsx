771e9ba9d0794b3d37a7c72f0f275270c1b2e4be
Carlos Guedes <cguedes@gmail.com>
2023-07-17 16:15:24 +0100

    Fix test!

--- src/components/__tests__/PasswordInput.test.tsx
+++ src/components/__tests__/PasswordInput.test.tsx
@@ -6,12 +6,12 @@ describe('PasswordInput', () => {
   it('should start as password toggle to text on "SHOW"/"HIDE" icon click', async () => {
     const { user } = setup(<PasswordInput placeholder="input password" value="some text" onChange={noop} />);
 
-    expect(screen.getByPlaceholderText<HTMLInputElement>('input password')).toBe('password');
+    expect(screen.getByPlaceholderText<HTMLInputElement>('input password').type).toBe('password');
 
     await user.click(screen.getByTitle('Show'));
-    expect(screen.getByPlaceholderText<HTMLInputElement>('input password')).toBe('text');
+    expect(screen.getByPlaceholderText<HTMLInputElement>('input password').type).toBe('text');
 
     await user.click(screen.getByTitle('Hide'));
-    expect(screen.getByPlaceholderText<HTMLInputElement>('input password')).toBe('password');
+    expect(screen.getByPlaceholderText<HTMLInputElement>('input password').type).toBe('password');
   });
 });
