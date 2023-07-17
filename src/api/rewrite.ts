2dfbc40b480db1ada088714680ac0820442f62f5
Carlos Guedes <cguedes@gmail.com>
2023-07-17 15:18:58 +0100

    Add rewrite options UI for the selection pane and API

--- src/api/rewrite.ts
+++ src/api/rewrite.ts
@@ -1,21 +1,43 @@
 import { notifyError } from '../notifications/notifications';
 import { callSidecar } from './sidecar';
 
-export async function askForRewrite(selection: string): Promise<string> {
+export const REWRITE_MANNER: RewriteOptions['manner'][] = ['concise', 'elaborate', 'scholarly'];
+export interface RewriteOptions {
+  nChoices?: number;
+  manner?: 'concise' | 'elaborate' | 'scholarly';
+  temperature?: number; // between 0.7 and 0.9
+}
+
+const DEFAULT_OPTIONS: RewriteOptions = {
+  nChoices: 3,
+  manner: 'concise',
+  temperature: 0.7,
+};
+
+export async function askForRewrite(selection: string, options?: RewriteOptions): Promise<string[]> {
   try {
     if (selection.trim() === '') {
-      return '';
+      return [''];
     }
 
-    const response = await callSidecar('rewrite', { text: selection });
+    options = {
+      ...DEFAULT_OPTIONS,
+      ...options,
+    };
+    const response = await callSidecar('rewrite', {
+      text: selection,
+      n_choices: options.nChoices,
+      temperature: options.temperature,
+      manner: options.manner,
+    });
 
     if (response.status === 'error') {
       notifyError('Rewrite error', response.message);
-      return '';
+      return [''];
     }
 
-    return response.choices[0].text;
+    return response.choices.map((ch) => ch.text);
   } catch (err) {
-    return `Rewrite error: ${String(err)}`;
+    return [`Rewrite error: ${String(err)}`];
   }
 }
