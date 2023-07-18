0ca777f2799d99d0066048d70cda89a07516e4cb
Carlos Guedes <cguedes@gmail.com>
2023-07-18 11:54:44 +0100

    Add splashscreen and load settings and project structure before app renders
    Note that the webkit white background is a pending tauri issue: https://github.com/tauri-apps/tauri/issues/1564

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import './index.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { invoke } from '@tauri-apps/api';
 import { Provider } from 'jotai';
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './application/App';
+import { useAsyncEffect } from './hooks/useAsyncEffect';
+import { ensureProjectFileStructure } from './io/filesystem';
+import { noop } from './lib/noop';
+import { notifyInfo } from './notifications/notifications';
+import { interceptConsoleMessages } from './notifications/notifications.console';
+import { initSettings } from './settings/settingsManager';
 
 const queryClient = new QueryClient();
 
@@ -13,8 +20,35 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AppStartup />
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 );
+
+function AppStartup() {
+  const [initialized, setInitialized] = useState(false);
+
+  useAsyncEffect(
+    async () => {
+      // Note: Intercepting INFO, WARN and ERROR console.*
+      interceptConsoleMessages(true, true, true);
+
+      notifyInfo('Application Startup');
+      await initSettings();
+      await ensureProjectFileStructure();
+      await invoke('close_splashscreen');
+      notifyInfo('Application Initialized');
+
+      setInitialized(true);
+    },
+    noop,
+    [setInitialized],
+  );
+
+  if (!initialized) {
+    return null;
+  }
+
+  return <App />;
+}
---
+++ src/splash.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { WelcomeView } from './application/views/WelcomeView';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WelcomeView className="h-screen bg-white" hideShortcuts />
+  </React.StrictMode>,
+);
