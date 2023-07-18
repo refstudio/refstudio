029c3a367f95c893fe9a42dd3d3ab03e998cfad3
Mathis Pierron <mathis.pierron@yld.io>
2023-07-18 11:50:48 +0100

    feat: allow escaping the citation node from the left

--- src/features/textEditor/components/tipTapNodes/citation/citationNode.ts
+++ src/features/textEditor/components/tipTapNodes/citation/citationNode.ts
@@ -141,7 +141,28 @@ export const citationNode = Node.create({
         if (!maybeNextNode) {
           tr.insert(resolvedAfterPos.pos, editor.schema.text(' '));
         }
-        tr.setSelection(TextSelection.create(tr.doc, resolvedAfterPos.pos + 1));
+        tr.setSelection(TextSelection.create(tr.doc, $from.after() + 1));
+
+        editor.view.dispatch(tr);
+        return true;
+      },
+      ArrowLeft: ({ editor }) => {
+        if (!editor.state.selection.empty) {
+          return false;
+        }
+
+        const { $from } = editor.state.selection;
+        if ($from.parent.type.name !== this.type.name || $from.pos !== $from.before() + 1) {
+          return false;
+        }
+
+        const resolvedBeforePos = editor.state.doc.resolve($from.before());
+        const maybePreviousNode = resolvedBeforePos.nodeBefore;
+        const { tr } = editor.state;
+        if (!maybePreviousNode) {
+          tr.insert(resolvedBeforePos.pos, editor.schema.text(' '));
+        }
+        tr.setSelection(TextSelection.create(tr.doc, $from.before()));
 
         editor.view.dispatch(tr);
         return true;
