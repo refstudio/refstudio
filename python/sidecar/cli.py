9b886e1cfe480f16e96f70bd845b69f1637baf70
shauryr <shauryr@gmail.com>
2023-07-18 19:13:09 -0500

    search parser added

--- python/sidecar/cli.py
+++ python/sidecar/cli.py
@@ -10,23 +10,62 @@ def get_arg_parser():
         "--debug",
         action="store_true",
     )
-    parser.add_argument(
-        'command',
-        # To add a new Sidecar command, add it to the CliCommands class.
-        choices=[*inspect.signature(CliCommands).parameters.keys()],
-    )
-    parser.add_argument(
-        'param_json',
-        type=str,
-        nargs='?',
-        default='null',
-        help='Command argument as JSON. Expected schema depends on the command.'
+    subparsers = parser.add_subparsers(
+        dest="command",
     )
 
-    search_parser = subparsers.add_parser(
-        "search",
-        description="Search for a paper"
+    ingest_parser = subparsers.add_parser(
+        "ingest",
+        description="Ingest PDFs",
     )
+    ingest_parser.add_argument(
+        "--pdf_directory",
+        type=str,
+    )
+
+    ingest_status_parser = subparsers.add_parser(  # noqa: F841
+        "ingest_status", description="Retrieve ingestion status of uploads"
+    )
+
+    ingest_references_parser = subparsers.add_parser(
+        "ingest_references", description="Retrieve ingested PDF references"
+    )
+    ingest_references_parser.add_argument(
+        "--pdf_directory",
+        type=str,
+    )
+
+    ai_parser = subparsers.add_parser(
+        "rewrite",
+        description="Rewrites a block of text in a more concise manner",
+    )
+    ai_parser.add_argument(
+        "--text",
+        type=str,
+    )
+
+    chat_parser = subparsers.add_parser(
+        "chat",
+        description="Chat with the AI",
+    )
+    chat_parser.add_argument(
+        "--text",
+        type=str,
+    )
+
+    update_parser = subparsers.add_parser(  # noqa: F481
+        "update", description="Update metadata for a Reference"
+    )
+    update_parser.add_argument(
+        "--data",
+        type=str,
+    )
+
+    delete_parser = subparsers.add_parser("delete", description="Deletes a Reference")
+    delete_parser.add_argument("--source_filenames", nargs="*", type=str)
+    delete_parser.add_argument("--all", action="store_true")
+
+    search_parser = subparsers.add_parser("search", description="Search for a paper")
 
     search_parser.add_argument(
         "--query",
