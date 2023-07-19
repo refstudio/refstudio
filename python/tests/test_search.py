1c1f3c655f335d76441f42591ad9efb2f8312e57
shauryr <shauryr@gmail.com>
2023-07-19 13:14:02 -0500

    test search

---
+++ python/tests/test_search.py
@@ -0,0 +1,47 @@
+from sidecar import search
+import json
+from sidecar.typing import SearchRequest, SearchResponse, S2SearchResult
+
+
+def test_search(monkeypatch, capsys):
+    def mock_search_paper(*args, **kwargs):
+        response = SearchResponse(
+            results=[
+                S2SearchResult(
+                    title="Sample Paper Title",
+                    abstract="Sample Abstract",
+                    venue="Sample Venue",
+                    year=2021,
+                    paperId="sample-id-1",
+                    citationCount=10,
+                    openAccessPdf={"url": "https://sample1.pdf", "status": "sample1"},
+                    authors=["author1", "author2", "author3"],
+                ),
+                S2SearchResult(
+                    title="Sample Paper Title 2",
+                    abstract="Sample Abstract 2",
+                    venue="Sample Venue 2",
+                    year=2022,
+                    paperId="sample-id-2",
+                    citationCount=20,
+                    openAccessPdf={"url": "https://sample2.pdf", "status": "sample2"},
+                    authors=["author1", "author2", "author3"],
+                ),
+            ]
+        )
+        return response
+
+    monkeypatch.setattr(search.Searcher, "search_func", mock_search_paper)
+
+    _ = search.search_s2(SearchRequest(query="any-query-string-you-like"))
+    captured = capsys.readouterr()
+    output = json.loads(captured.out)
+
+    assert len(output) == 2
+    assert output["results"][0]["title"] == "Sample Paper Title"
+    assert output["results"][0]["authors"][0] == "author1"
+    assert output["results"][0]["authors"][1] == "author2"
+    assert output["results"][0]["authors"][2] == "author3"
+    assert output["results"][1]["title"] == "Sample Paper Title 2"
+    assert output["results"][1]["authors"][0] == "author1"
+    assert output["results"][1]["authors"][1] == "author2"
