3dc1693f6f63d9d4d279c86d051e6a2f22a6945a
Mathis Pierron <58954208+sehyod@users.noreply.github.com>
2023-07-20 13:17:28 +0100

    fix: hide splashscreen instead of closing (#314)
    Co-authored-by: Mathis Pierron <mathis.pierron@yld.io>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,7 +11,7 @@ fn get_environment_variable(name: &str) -> String {
 async fn close_splashscreen(window: tauri::Window) {
     // Close splashscreen
     if let Some(splashscreen) = window.get_window("splashscreen") {
-        splashscreen.close().unwrap();
+        splashscreen.hide().unwrap();
     }
     // Show main window
     window.get_window("main").unwrap().show().unwrap();
