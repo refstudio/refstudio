5b44e6a949fea08a1cf2b83490ec6b9d5a2e8749
Carlos Guedes <cguedes@gmail.com>
2023-07-21 10:17:28 +0100

    Remove unused

--- src/application/commands/CommandPaletteConfigs.tsx
+++ src/application/commands/CommandPaletteConfigs.tsx
@@ -2,7 +2,6 @@ export const INDEX_MAIN = 1;
 export const INDEX_REFERENCES = 20;
 export const INDEX_FILES = 30;
 export const INDEX_REWRITE_WIDGET = 40;
-export const INDEX_REWRITE_MENUS = 50;
 
 // " " will hide the placeholder (via CSS)
 export const HIDE_PLACEHOLDER_TEXT = ' ';
