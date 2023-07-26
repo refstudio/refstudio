2cfd00626b5989a0630576a1d8b665fef0876219
Mathis Pierron <mathis.pierron@yld.io>
2023-07-26 12:51:41 +0100

    chore: remove deprecated draggable and collapsible blocks

--- src/features/textEditor/components/tipTapNodes/refStudioDocument/RefStudioDocument.ts
+++ src/features/textEditor/components/tipTapNodes/refStudioDocument/RefStudioDocument.ts
@@ -1,35 +1,5 @@
 import Document from '@tiptap/extension-document';
 
-import { unsetPartiallySelectedCollapsibleBlocks } from '../collapsibleBlock/helpers/unsetPartiallySelectedCollapsibleBlocks';
-
-declare module '@tiptap/core' {
-  interface Commands<ReturnType> {
-    refStudioDocument: {
-      deleteNonEmptySelection: () => ReturnType;
-    };
-  }
-}
-
 export const RefStudioDocument = Document.extend({
   content: 'notionBlock* | codeBlock',
-  addKeyboardShortcuts: () => ({
-    // Backspace: backspace,
-    Tab: () => true,
-    'Shift-Tab': () => true,
-    'Mod-l': ({ editor }) => {
-      console.log(editor.state.selection.from);
-      console.log(editor.getHTML());
-      return true;
-    },
-  }),
-  addCommands: () => ({
-    deleteNonEmptySelection: () => (props) => {
-      const { dispatch, tr } = props;
-      if (dispatch) {
-        unsetPartiallySelectedCollapsibleBlocks(props);
-        tr.deleteSelection();
-      }
-      return true;
-    },
-  }),
 });
