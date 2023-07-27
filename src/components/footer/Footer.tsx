efb6fbe3e8c5165f1ba22952c4cc00892511eaf8
Carlos Guedes <cguedes@gmail.com>
2023-07-27 14:54:30 +0100

    Display project dir in the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,26 @@
+import { VscHome } from 'react-icons/vsc';
+
 import { ReferencesFooterItems } from '../../features/references/footer/ReferencesFooterItems';
+import { getProjectBaseDir } from '../../io/filesystem';
 import { FooterNotificationItems } from '../../notifications/footer/FooterNotificationItems';
+import { FooterItem } from './FooterItem';
 
 export function Footer() {
   return (
     <div className="relative flex items-center justify-end gap-1 bg-black pl-2 text-white">
+      <ProjectFooterItems />
       <ReferencesFooterItems />
       <FooterNotificationItems />
     </div>
+  );
+}
+
+function ProjectFooterItems() {
+  const projectDir = getProjectBaseDir();
+
+  return (
+    <>
+      <FooterItem className="mr-auto" icon={<VscHome />} text={projectDir} />
+    </>
   );
 }
