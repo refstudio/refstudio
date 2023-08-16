feac4909416e304aa9e93be83368a5bfc52c686a
Dan Vanderkam <danvdk@gmail.com>
2023-08-16 12:46:19 -0400

    special-case get_environment_variable

--- src/wrappers/tauri-wrapper.ts
+++ src/wrappers/tauri-wrapper.ts
@@ -2,6 +2,9 @@ import { invoke as tauriInvoke } from '@tauri-apps/api';
 
 export const invoke: typeof tauriInvoke = (cmd, args) => {
   console.log('invoke', cmd, args);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  if (cmd === 'get_environment_variable') {
+    return '';
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any
   return null as any;
 };
