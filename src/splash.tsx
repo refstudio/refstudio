1dc33989d603cebbb71edc80a85390f92ccf23b6
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-08-22 14:44:08 +0100

    fix: use correct background color in splash screen

--- src/splash.tsx
+++ src/splash.tsx
@@ -7,7 +7,7 @@ import { Logo } from './application/views/Logo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center bg-grayscale-10">
       <Logo />
     </div>
   </React.StrictMode>,
