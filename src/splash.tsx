216f8a389e500cfbed065ab2f79a1f1ca9fffc3f
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-08-23 12:58:48 +0100

    chore: update tailwind color names

--- src/splash.tsx
+++ src/splash.tsx
@@ -7,7 +7,7 @@ import { Logo } from './application/views/Logo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="flex h-full w-full items-center justify-center bg-grayscale-10">
+    <div className="flex h-full w-full items-center justify-center bg-content-area-bg-primary">
       <Logo />
     </div>
   </React.StrictMode>,
