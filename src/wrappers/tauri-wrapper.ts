2c5acedb76980f025cc03311a73d6fabeac33ca4
Carlos Guedes <cguedes@gmail.com>
2023-08-25 11:38:10 +0100

    Remove legacy fs.ts and stub

--- src/wrappers/tauri-wrapper.ts
+++ src/wrappers/tauri-wrapper.ts
@@ -10,7 +10,6 @@ import * as tauriFs from '@tauri-apps/api/fs';
 import * as tauriPath from '@tauri-apps/api/path';
 
 import * as stubEvent from './tauri-api-stubs/event';
-import * as stubFs from './tauri-api-stubs/fs';
 import { invokeStub } from './tauri-api-stubs/invokeStub';
 import * as stubPath from './tauri-api-stubs/path';
 
@@ -24,17 +23,21 @@ export const join = import.meta.env.VITE_IS_WEB ? stubPath.join : tauriPath.join
 export const sep = import.meta.env.VITE_IS_WEB ? stubPath.sep : tauriPath.sep;
 
 // @tauri-apps/api/fs
-export const createDir = import.meta.env.VITE_IS_WEB ? stubFs.createDir : tauriFs.createDir;
-export const exists = import.meta.env.VITE_IS_WEB ? stubFs.exists : tauriFs.exists;
-export const readBinaryFile = import.meta.env.VITE_IS_WEB ? stubFs.readBinaryFile : tauriFs.readBinaryFile;
-export const readTextFile = import.meta.env.VITE_IS_WEB ? stubFs.readTextFile : tauriFs.readTextFile;
-export const readDir = import.meta.env.VITE_IS_WEB ? stubFs.readDir : tauriFs.readDir;
-export const removeDir = import.meta.env.VITE_IS_WEB ? stubFs.removeDir : tauriFs.removeDir;
-export const removeFile = import.meta.env.VITE_IS_WEB ? stubFs.removeFile : tauriFs.removeFile;
-export const writeTextFile = import.meta.env.VITE_IS_WEB ? stubFs.writeTextFile : tauriFs.writeTextFile;
-export const writeBinaryFile = import.meta.env.VITE_IS_WEB ? stubFs.writeBinaryFile : tauriFs.writeBinaryFile;
-export const renameFile = import.meta.env.VITE_IS_WEB ? stubFs.renameFile : tauriFs.renameFile;
+export const createDir = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.createDir;
+export const exists = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.exists;
+export const readBinaryFile = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.readBinaryFile;
+export const readTextFile = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.readTextFile;
+export const readDir = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.readDir;
+export const removeDir = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.removeDir;
+export const removeFile = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.removeFile;
+export const writeTextFile = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.writeTextFile;
+export const writeBinaryFile = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.writeBinaryFile;
+export const renameFile = import.meta.env.VITE_IS_WEB ? dontUseFsInWeb : tauriFs.renameFile;
 
 // @tauri-apps/api/event
 export const emit = import.meta.env.VITE_IS_WEB ? stubEvent.emit : tauriEvent.emit;
 export const listen = import.meta.env.VITE_IS_WEB ? stubEvent.listen : tauriEvent.listen;
+
+function dontUseFsInWeb() {
+  throw new Error('Dont use FS in the WEB. Use the HTTP API instead');
+}
