b34ee7d3b261c65f9859ab13ce77a81bec13cd26
Jessica Voytek <jessica.voytek@gmail.com>
2023-08-30 12:19:34 -0700

    New design for reference editor, more granular component architecture, more inline utility classes

--- src/features/references/editor/ReferencesTableView.tsx
+++ src/features/references/editor/ReferencesTableView.tsx
@@ -1,6 +1,5 @@
 import '@ag-grid-community/styles/ag-grid.css';
 import '@ag-grid-community/styles/ag-theme-alpine.css';
-import './ReferenceView.css';
 
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 import {
