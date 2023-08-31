4539583269236b887d06acbba28c8738d53befaf
Dan Vanderkam <danvdk@gmail.com>
2023-08-31 13:05:23 -0400

    test passes

--- src/api/__tests__/typed-api.test.ts
+++ src/api/__tests__/typed-api.test.ts
@@ -1,5 +1,19 @@
+import { universalGet } from '../api';
+import { apiGetJson } from '../typed-api';
+
+vi.mock('../api');
+
 describe('Typed API', () => {
-  it('should issue GET requests with path parameters', () => {
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should issue GET requests with path parameters', async () => {
+    vi.mocked(universalGet).mockResolvedValueOnce(JSON.stringify('text file'));
+    const response = await apiGetJson('/api/fs/{project_id}/{filepath}', {
+      path: { project_id: 'abc', filepath: 'foo/bar.txt' },
+    });
+    expect(response).toBe('"text file"');
+    expect(universalGet).toHaveBeenCalledWith('/api/fs/abc/foo/bar.txt', undefined);
   });
 });
