334b25b0469115242a8d2e3a5d08f712faed12e9
Dan Vanderkam <danvdk@gmail.com>
2023-08-31 13:26:37 -0400

    docs for codegen_cleanup.py

--- python/codegen_cleanup.py
+++ python/codegen_cleanup.py
@@ -1,4 +1,14 @@
-"""Clean up some of the generated types."""
+"""Clean up some of the generated types from generate_schema.py.
+
+After running that script in codegen.sh, we have two generated TS files:
+
+- api-types.ts, which defines interfaces for all request/response types.
+- api-paths.ts, which defines our full HTTP API, including all request/response types.
+
+The types in api-types.ts are nicer to work with. This script patches api-paths.ts to
+refer to the types in api-types.ts. This requires some normalization becuase json2ts
+and openapi-typescript convert Python class names to TS types names a bit differently.
+"""
 
 import re
 
@@ -8,9 +18,8 @@ def normalize_typename(typename: str):
 
 
 if __name__ == "__main__":
-    # Remove `components` from api-paths.ts
-    # Create `components` in api-types.ts
-
+    # Pull out all the named types/interfaces in api-types.ts.
+    # Not all of these will be used by api-paths.ts.
     api_type_lines = open("src/api/api-types.ts").readlines()
     api_types = []
     for line in api_type_lines:
@@ -25,11 +34,14 @@ if __name__ == "__main__":
         normalize_typename(typename): typename for typename in api_types
     }
 
+    # The types we want to update in api-paths.ts look like
+    # components['schemas']['NameOfType']
     api_paths = open("src/api/api-paths.ts").read()
     component_schemas = set()
     for m in re.finditer(r"components\['schemas'\]\['([^']+)'\]", api_paths):
         component_schemas.add(m.group(1))
 
+    # Update references in api-paths.ts, tracking the types we need to import.
     used_types = set()
     for schema in component_schemas:
         api_typename = normname_to_api_name[normalize_typename(schema)]
