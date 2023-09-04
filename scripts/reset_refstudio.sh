9c3b913c5f6bbe085604ff89f82a4c257ad07d7e
Carlos Guedes <cguedes@users.noreply.github.com>
2023-09-04 15:57:20 +0100

    chore: Add a reset_refstudio.sh script and update README.md (#477)
    Add a reset_refstudio.sh script and update README.md

---
+++ scripts/reset_refstudio.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# show help for the command -h
+if [[ $1 == "-h" ]]; then
+    echo "Usage: reset_refstudio.sh -h"
+    echo "Resets the RefStudio for the WEB_STORAGE_URL (defaults to /tmp/web-storage-url) folder"
+    echo ""
+    echo "WEB_STORAGE_URL=/some/path reset_refstudio.sh"
+    echo "This will reset the RefStudio for the /some/path folder"
+    exit 0
+fi
+
+
+# set variable with fallback to environment variable
+WEB_STORAGE_URL=${WEB_STORAGE_URL:-"/tmp/web-storage-url"}
+
+# Confirm if the user want to perform the operation
+read -p "Are you sure you want to reset the RefStudio? [y/N] " -n 1 -r
+
+# exit if the user does not confirm
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Operation aborted"
+    exit 1
+fi
+
+# Backup settings file to a temporary location, if file exists
+if [ -f "$WEB_STORAGE_URL/user1/settings.json" ]; then
+    cp "$WEB_STORAGE_URL/user1/settings.json" "$WEB_STORAGE_URL/user1.settings.json.bak"
+fi
+
+# Remove all files from the user1 folder
+rm -rf "$WEB_STORAGE_URL/user1"
+
+# Inform the user that the operation was successful for the WEB_STORAGE_URL folder
+echo ""
+echo "RefStudio reset successfully for the $WEB_STORAGE_URL folder"
