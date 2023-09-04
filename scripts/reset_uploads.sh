2e0f8bf310b9d0e6f909bab115fcf45e44706450
Carlos Guedes <cguedes@gmail.com>
2023-09-04 13:39:41 +0100

    Add a reset_refstudio.sh script and update README.md

--- scripts/reset_uploads.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-PROJECT_DIR="$HOME/Library/Application Support/studio.ref.desktop/project-x"
-
-rm -f "$PROJECT_DIR/uploads/"*.pdf
-rm -rf "$PROJECT_DIR/.staging"
-rm -rf "$PROJECT_DIR/.grobid"
-rm -rf "$PROJECT_DIR/.storage"
