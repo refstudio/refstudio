f346deecae81f2be35e6089606b810a3b49d2e21
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-09-05 10:15:47 +0100

    fix: remove warning

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -7,7 +7,7 @@ interface RadioProps {
 export function Checkbox({ checked, onChange, ...rest }: RadioProps) {
   return (
     <div className="cursor-pointer" onClick={() => onChange(!checked)} {...rest}>
-      <input checked={checked} className="hidden" role="checkbox" type="checkbox" />
+      <input checked={checked} className="hidden" readOnly role="checkbox" type="checkbox" />
       {checked ? (
         <div className="text-radio-active">
           <SelectedRadioIcon />
