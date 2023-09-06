16c28e49704ee37a4ecabb5ad2181130498dabb7
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-09-06 14:06:19 +0100

    chore: remove unused export

--- src/application/components/icons.tsx
+++ src/application/components/icons.tsx
@@ -8,19 +8,6 @@ export function AddIcon() {
   );
 }
 
-export function SampleIcon() {
-  return (
-    <div className="flex h-6 w-6 items-center justify-center">
-      <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M7 2V4H8V18C8 19.0609 8.42143 20.0783 9.17157 20.8284C9.92172 21.5786 10.9391 22 12 22C13.0609 22 14.0783 21.5786 14.8284 20.8284C15.5786 20.0783 16 19.0609 16 18V4H17V2H7ZM11 16C10.4 16 10 15.6 10 15C10 14.4 10.4 14 11 14C11.6 14 12 14.4 12 15C12 15.6 11.6 16 11 16ZM13 12C12.4 12 12 11.6 12 11C12 10.4 12.4 10 13 10C13.6 10 14 10.4 14 11C14 11.6 13.6 12 13 12ZM14 7H10V4H14V7Z"
-          fill="currentcolor"
-        />
-      </svg>
-    </div>
-  );
-}
-
 export const RefStudioEditorIcon = () => (
   <div className="flex h-6 w-6 items-center justify-center">
     <svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
