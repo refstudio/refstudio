ef09794dbcadcf848eb37f1f3d253c87c4a1b642
Carlos Guedes <cguedes@gmail.com>
2023-09-06 13:32:21 +0100

    Add missing unit tests

--- src/application/components/__tests__/ProjectsList.test.tsx
+++ src/application/components/__tests__/ProjectsList.test.tsx
@@ -9,7 +9,7 @@ describe('ProjectsList', () => {
   });
 
   it('should render the list of projects', () => {
-    render(<ProjectsList header="Recent Projects" projects={PROJECTS} onProjectClick={noop} />);
+    render(<ProjectsList projects={PROJECTS} onProjectClick={noop} />);
 
     expect(screen.getAllByRole('menuitem')).toHaveLength(PROJECTS.length);
     PROJECTS.forEach((project) => {
@@ -17,13 +17,23 @@ describe('ProjectsList', () => {
     });
   });
 
+  it('should render an empty list of projects', () => {
+    render(<ProjectsList projects={[]} onProjectClick={noop} />);
+    expect(screen.queryByRole('menuitem')).not.toBeInTheDocument();
+  });
+
   it('should call handler with the project clicked on', async () => {
     const fn = vi.fn();
-    const { user } = setup(<ProjectsList header="Recent Projects" projects={PROJECTS} onProjectClick={fn} />);
+    const { user } = setup(<ProjectsList projects={PROJECTS} onProjectClick={fn} />);
 
     await user.click(screen.getByText(PROJECTS[1].name));
 
     expect(fn).toHaveBeenCalledTimes(1);
     expect(fn).toHaveBeenCalledWith(PROJECTS[1]);
+  });
+
+  it('should render the configured header', () => {
+    render(<ProjectsList header="Recent Projects" projects={[]} onProjectClick={noop} />);
+    expect(screen.getByText('Recent Projects')).toBeInTheDocument();
   });
 });
