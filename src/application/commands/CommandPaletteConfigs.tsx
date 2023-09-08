ca16ddd4653b88c0b575092b3f9351784a2fb280
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-09-08 17:10:32 +0100

    fix: run prettier

--- src/application/commands/CommandPaletteConfigs.tsx
+++ src/application/commands/CommandPaletteConfigs.tsx
@@ -1,6 +1,3 @@
 export const INDEX_MAIN = 1;
 export const INDEX_REFERENCES = 20;
 export const INDEX_FILES = 30;
-
-// " " will hide the placeholder (via CSS)
-export const HIDE_PLACEHOLDER_TEXT = ' ';
--- src/atoms/hooks/useActivePaneId.ts
+++
@@ -1,7 +0,0 @@
-import { useAtomValue } from 'jotai';
-
-import { activePaneIdAtom } from '../core/activePane';
-
-export function useActivePaneId() {
-  return useAtomValue(activePaneIdAtom);
-}
