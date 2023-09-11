97988ddc778b0640e158b91d1115b6eb97c62407
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-09-11 16:00:08 +0100

    fix: make creativity tooltip centered

--- src/features/components/CreativityInfoTooltip.tsx
+++ src/features/components/CreativityInfoTooltip.tsx
@@ -6,20 +6,20 @@ import { InfoIcon } from './icons';
 export function CreativityInfoTooltip({ id }: { id: string }) {
   return (
     <div>
-      <div className="text-btn-ico-tool-active" id={id}>
+      <div className="text-btn-bg-info-pop-up" id={id}>
         <InfoIcon />
       </div>
       <Tooltip
         anchorSelect={`#${id}`}
         className={cx(
           'rounded flex flex-col items-stretch px-4 py-3 relative z-tooltip',
-          'max-w-[16rem] bg-pop-up-message-bg -translate-x-6',
+          'max-w-[16rem] bg-pop-up-message-bg',
         )}
-        classNameArrow='translate-x-6 w-2 h-2 rotate-45'
+        classNameArrow='w-2 h-2 rotate-45'
         clickable
+        delayShow={100}
         disableStyleInjection
         opacity={100}
-        openOnClick
         positionStrategy="fixed"
       >
         <div className="text-pop-up-message-txt">
