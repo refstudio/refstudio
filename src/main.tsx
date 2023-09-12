3704f4a63d2788adbd7de4adac313472ae7a9ff6
Carlos Guedes <cguedes@gmail.com>
2023-09-12 09:58:54 +0100

    Remove test App code

--- src/main.tsx
+++ src/main.tsx
@@ -18,28 +18,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Provider>
   </React.StrictMode>,
 );
-
-// function App() {
-//   const [start, setStart] = useState(false);
-
-//   return (
-//     <div>
-//       <h1>Hello Tauri!</h1>
-//       <button
-//         className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-//         onClick={() => {
-//           setStart(true);
-//         }}
-//       >
-//         Start server
-//       </button>
-//       {start && <ServerStart />}
-//     </div>
-//   );
-// }
-
-// function ServerStart() {
-//   const isServerRunning = useRefStudioServerOnDesktop();
-//   console.log('isServerRunning=', isServerRunning);
-//   return <div>isServerRunning: {isServerRunning ? 'TRUE' : 'FALSE'}</div>;
-// }
