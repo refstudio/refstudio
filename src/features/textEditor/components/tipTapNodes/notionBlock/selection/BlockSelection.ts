8125fe4bbe6f2d17f37e0fa7361dcae324fc144d
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-09-14 14:29:00 +0100

    fix: remove duplicated code

--- src/features/textEditor/components/tipTapNodes/notionBlock/selection/BlockSelection.ts
+++ src/features/textEditor/components/tipTapNodes/notionBlock/selection/BlockSelection.ts
@@ -1,5 +1,5 @@
 import { Node, ResolvedPos, Slice } from '@tiptap/pm/model';
-import { Selection, TextSelection, Transaction } from '@tiptap/pm/state';
+import { Selection, Transaction } from '@tiptap/pm/state';
 import { Mappable } from '@tiptap/pm/transform';
 
 import { getNextSibling } from '../utils/getNextSibling';
@@ -26,8 +26,9 @@ export class BlockSelection extends Selection {
   }
 
   map(doc: Node, mapping: Mappable): Selection {
-    const $from = doc.resolve(mapping.map(this.from));
-    return TextSelection.near($from);
+    const $anchor = doc.resolve(mapping.map(this.anchor));
+    const $head = doc.resolve(mapping.map(this.head));
+    return new BlockSelection($anchor, $head);
   }
 
   content(): Slice {
@@ -76,83 +77,43 @@ export class BlockSelection extends Selection {
   }
 
   selectParentBlock(tr: Transaction, dispatch: (tr: Transaction) => void): boolean {
-    const parent = getParent(this.$head);
-    if (!parent) {
-      // If the node is the first node of the document, selectPreviousBlock does nothing
+    const { parentBlock } = this;
+    if (!parentBlock) {
       return false;
     }
-    tr.setSelection(new BlockSelection(parent.resolvedPos));
+    tr.setSelection(new BlockSelection(parentBlock.resolvedPos));
     dispatch(tr);
     return true;
   }
 
   selectChildBlock(tr: Transaction, dispatch: (tr: Transaction) => void): boolean {
-    const doc = this.$head.node(0);
-    const currentBlock = doc.nodeAt(this.head)!;
-    if ((currentBlock.attrs.type !== 'collapsible' || !currentBlock.attrs.collapsed) && currentBlock.childCount > 1) {
-      const childPos = this.head + currentBlock.firstChild!.nodeSize + 1;
-      const resolvedChildPos = doc.resolve(childPos);
-
-      tr.setSelection(new BlockSelection(resolvedChildPos));
-      dispatch(tr);
-      return true;
+    const { childBlock } = this;
+    if (!childBlock) {
+      return false;
     }
-    return false;
+    tr.setSelection(new BlockSelection(childBlock.resolvedPos));
+    dispatch(tr);
+    return true;
   }
 
   selectNextBlock(tr: Transaction, dispatch: (tr: Transaction) => void): boolean {
-    // If the selected block is not collapsed and has a child, select it
-    const success = this.selectChildBlock(tr, dispatch);
-    if (success) {
-      return true;
+    const { childBlock, nextBlock } = this;
+    if (!childBlock && !nextBlock) {
+      return false;
     }
 
-    // Otherwise select the next sibling
-    let currentResolvedPos = this.$head;
-    let nextBlock = getNextSibling(currentResolvedPos);
-    while (!nextBlock) {
-      const parent = getParent(currentResolvedPos);
-      if (!parent) {
-        // If the node is at the end of the document, selectNextBlock does nothing
-        return false;
-      }
-      currentResolvedPos = parent.resolvedPos;
-      nextBlock = getNextSibling(currentResolvedPos);
-    }
-    tr.setSelection(new BlockSelection(nextBlock.resolvedPos));
+    tr.setSelection(new BlockSelection((childBlock ?? nextBlock)!.resolvedPos));
     dispatch(tr);
     return true;
   }
 
   selectPreviousBlock(tr: Transaction, dispatch: (tr: Transaction) => void): boolean {
-    const currentResolvedPos = this.$head;
-    const previousSibling = getPreviousSibling(currentResolvedPos);
-
-    // If the block is its parent's first child, return the parent
-    if (!previousSibling) {
-      return this.selectParentBlock(tr, dispatch);
+    const { previousOrParentBlock } = this;
+    if (!previousOrParentBlock) {
+      return false;
     }
 
-    // Otherwise select the deepest child of the previous sibling
-    const doc = this.$head.node(0);
-    let previousBlock = previousSibling;
-    while (
-      (previousBlock.node.attrs.type !== 'collapsible' || !previousBlock.node.attrs.collapsed) &&
-      previousBlock.node.childCount > 1
-    ) {
-      let lastChildOffset = 1;
-      previousBlock.node.forEach((_child, offset, index) => {
-        if (index === previousBlock.node.childCount - 1) {
-          lastChildOffset += offset;
-        }
-      });
-      previousBlock = {
-        node: previousBlock.node.lastChild!,
-        resolvedPos: doc.resolve(previousBlock.resolvedPos.pos + lastChildOffset),
-      };
-    }
-
-    tr.setSelection(new BlockSelection(previousBlock.resolvedPos));
+    tr.setSelection(new BlockSelection(previousOrParentBlock.resolvedPos));
     dispatch(tr);
     return true;
   }
@@ -168,7 +129,7 @@ export class BlockSelection extends Selection {
   }
 
   extendUp(tr: Transaction, dispatch: (tr: Transaction) => void): boolean {
-    const { previousBlock } = this;
+    const { previousOrParentBlock: previousBlock } = this;
     const doc = this.$head.node(0);
     if (previousBlock) {
       let newResolvedHead = previousBlock.resolvedPos;
@@ -226,7 +187,7 @@ export class BlockSelection extends Selection {
     return nextBlock;
   }
 
-  private get previousBlock(): NodeData | null {
+  private get previousOrParentBlock(): NodeData | null {
     const currentResolvedPos = this.$head;
     const previousSibling = getPreviousSibling(currentResolvedPos);
 
