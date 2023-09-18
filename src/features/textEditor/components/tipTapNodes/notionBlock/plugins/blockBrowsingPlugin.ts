d71bfe577399199432f3fcfff55269cc5db64d3d
Mathis Pierron <mathis.pierron@student-cs.fr>
2023-09-18 16:44:58 +0100

    feat: implement moving blocks down

--- src/features/textEditor/components/tipTapNodes/notionBlock/plugins/blockBrowsingPlugin.ts
+++ src/features/textEditor/components/tipTapNodes/notionBlock/plugins/blockBrowsingPlugin.ts
@@ -1,6 +1,6 @@
 import { Fragment, Slice } from '@tiptap/pm/model';
 import { Plugin, PluginKey, TextSelection } from '@tiptap/pm/state';
-import { ReplaceAroundStep, ReplaceStep } from '@tiptap/pm/transform';
+import { ReplaceAroundStep } from '@tiptap/pm/transform';
 import { Decoration, DecorationSet } from '@tiptap/pm/view';
 
 import { addUnindentSteps } from '../commands/unindent';
@@ -89,36 +89,120 @@ export const blockBrowsingPlugin = new Plugin<BlockBrowsingPluginState>({
           return;
         }
         case 'ArrowDown': {
-          if (!event.shiftKey) {
-            selection.selectNextBlock(tr, (t) => view.dispatch(t));
-          } else {
-            selection.expandDown(tr, (t) => view.dispatch(t));
-          }
-          return;
-        }
-        case 'ArrowUp': {
           if (event.shiftKey && event.metaKey) {
-            const indexInParent = selection.$from.indexAfter();
-            const { parent } = selection.$from;
+            // Cmd+Shift+Down: Move selected blocks down
+            const indexInParent = selection.$to.indexAfter();
+            const { parent } = selection.$to;
             const depthDifference = selection.$from.depth - selection.$to.depth;
             const endPos = selection.to + tr.doc.nodeAt(selection.to)!.nodeSize; // end of selection
             const selectionRange = selection.to - selection.from - depthDifference;
 
-            // Cmd+Shift+Up: Move selection up
-            // Bring the first node back to the same level as other nodes if needed
+            if (endPos === tr.doc.content.size) {
+              return;
+            }
+
+            // Bring the first nodes back to the same level as other nodes if needed
             if (selection.$from.depth > selection.$to.depth) {
               const blocksToUnindent = selection.selectedBlocksPos
                 .filter(({ from }) => tr.doc.resolve(from).depth > selection.$to.depth)
                 .reverse();
 
               for (const block of blocksToUnindent) {
-                for (let i = 0; i < depthDifference; i++) {
+                for (let i = 0; i < tr.doc.resolve(block.from).depth - selection.$to.depth; i++) {
                   addUnindentSteps(tr, tr.mapping.map(block.from + 2));
                 }
               }
             }
 
-            // If the current block has a sibling before itself, make it a child of this sibling
+            // If the last selected node has a sibling
+            if (indexInParent < parent.childCount - 1) {
+              const sibling = parent.child(indexInParent + 1);
+              // If the sibling is collapsed, move selection after the sibling
+              if (sibling.attrs.type === 'collapsible' && sibling.attrs.collapsed) {
+                const sliceToMove = tr.doc.slice(selection.from + depthDifference, endPos);
+
+                tr.delete(selection.from + depthDifference, endPos);
+
+                const siblingEndPos = tr.mapping.map(endPos + sibling.nodeSize);
+                tr.replace(siblingEndPos, siblingEndPos, sliceToMove);
+
+                if (selection.from === selection.anchor) {
+                  tr.setSelection(
+                    new BlockSelection(tr.doc.resolve(siblingEndPos), tr.doc.resolve(siblingEndPos + selectionRange)),
+                  );
+                } else {
+                  tr.setSelection(
+                    new BlockSelection(tr.doc.resolve(siblingEndPos + selectionRange), tr.doc.resolve(siblingEndPos)),
+                  );
+                }
+              } else {
+                // If the sibling is uncollapsed, make selected blocks its child
+                const sliceToMove = tr.doc.slice(selection.from + depthDifference, endPos);
+
+                tr.delete(selection.from + depthDifference, endPos);
+                const insertPos = tr.mapping.map(endPos + 1 + sibling.child(0)!.nodeSize);
+                tr.replace(insertPos, insertPos, sliceToMove);
+
+                if (selection.from === selection.anchor) {
+                  tr.setSelection(
+                    new BlockSelection(tr.doc.resolve(insertPos), tr.doc.resolve(insertPos + selectionRange)),
+                  );
+                } else {
+                  tr.setSelection(
+                    new BlockSelection(tr.doc.resolve(insertPos + selectionRange), tr.doc.resolve(insertPos)),
+                  );
+                }
+              }
+            } else if (selection.$to.depth > 0) {
+              // If the selected block has no sibling, unindent it
+              let fragment = Fragment.from(parent.copy());
+              for (let i = 0; i < depthDifference; i++) {
+                fragment = Fragment.from(parent.copy(fragment));
+              }
+              tr.step(
+                new ReplaceAroundStep(
+                  selection.from,
+                  endPos + 1,
+                  selection.from + depthDifference,
+                  endPos,
+                  new Slice(fragment, depthDifference + 1, 0),
+                  depthDifference + 1,
+                ),
+              );
+            }
+
+            view.dispatch(tr);
+          } else if (event.shiftKey) {
+            // Shift+Down: Expand selection down
+            selection.expandDown(tr, (t) => view.dispatch(t));
+          } else {
+            // Down: Select next block
+            selection.selectNextBlock(tr, (t) => view.dispatch(t));
+          }
+          return;
+        }
+        case 'ArrowUp': {
+          if (event.shiftKey && event.metaKey) {
+            // Cmd+Shift+Up: Move selected blocks up
+            const indexInParent = selection.$from.indexAfter();
+            const { parent } = selection.$from;
+            const depthDifference = selection.$from.depth - selection.$to.depth;
+            const endPos = selection.to + tr.doc.nodeAt(selection.to)!.nodeSize; // end of selection
+            const selectionRange = selection.to - selection.from - depthDifference;
+
+            // Bring the first nodes back to the same level as other nodes if needed
+            if (selection.$from.depth > selection.$to.depth) {
+              const blocksToUnindent = selection.selectedBlocksPos
+                .filter(({ from }) => tr.doc.resolve(from).depth > selection.$to.depth)
+                .reverse();
+
+              for (const block of blocksToUnindent) {
+                for (let i = 0; i < tr.doc.resolve(block.from).depth - selection.$to.depth; i++) {
+                  addUnindentSteps(tr, tr.mapping.map(block.from + 2));
+                }
+              }
+            }
+
             if (
               indexInParent > 0 &&
               // For NotionBlockNodes, the first child is an inline block so the index must be greater than 1
@@ -194,7 +278,7 @@ export const blockBrowsingPlugin = new Plugin<BlockBrowsingPluginState>({
             // Shift+Up: Expand selection up
             selection.expandUp(tr, (t) => view.dispatch(t));
           } else {
-            // Up: Selects previous block
+            // Up: Select previous block
             selection.selectPreviousBlock(tr, (t) => view.dispatch(t));
           }
           return;
