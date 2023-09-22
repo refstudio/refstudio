70fa7c64bb69297b5ad223344236984de043234f
Carlos Guedes <cguedes@gmail.com>
2023-09-22 10:05:59 +0100

    refreshFileTree when adding s2 reference

--- src/atoms/s2SearchState.ts
+++ src/atoms/s2SearchState.ts
@@ -3,6 +3,7 @@ import { atom } from 'jotai';
 import { S2SearchResult } from '../api/api-types';
 import { removeProjectReferences } from '../api/referencesAPI';
 import { getS2ReferencesByKeyword, postS2Reference } from '../api/s2SearchAPI';
+import { refreshFileTreeAtom } from './fileExplorerActions';
 import { loadReferencesAtom } from './referencesState';
 
 // #####################################################################################
@@ -28,6 +29,7 @@ export const clearSearchResultsAtom = atom(null, (_get, set) => {
 export const addS2ReferenceAtom = atom(null, async (_get, set, projectId: string, s2SearchResult: S2SearchResult) => {
   const status = await postS2Reference(projectId, s2SearchResult);
   await set(loadReferencesAtom, projectId);
+  await set(refreshFileTreeAtom);
   return status;
 });
 
