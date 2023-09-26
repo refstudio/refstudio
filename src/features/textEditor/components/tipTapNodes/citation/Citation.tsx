49253824855452d088040066b4735aebf60b6454
Mathis Pierron <58954208+sehyod@users.noreply.github.com>
2023-09-26 17:29:41 +0100

    feat: remove restrictions on citations (#593)
    * feat: remove citation nodes
    
    * chore: remove unused code
    
    * fix: update tests

--- src/features/textEditor/components/tipTapNodes/citation/Citation.tsx
+++
@@ -1,9 +0,0 @@
-import { NodeViewContent, NodeViewWrapper } from '@tiptap/react';
-
-export function Citation() {
-  return (
-    <NodeViewWrapper as="span">
-      [<NodeViewContent as="span" />]
-    </NodeViewWrapper>
-  );
-}
